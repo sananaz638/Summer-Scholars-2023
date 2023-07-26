8072288ae660e445a7813e15047aebfa87bb08cc
sananaz638 <139829179+sananaz638@users.noreply.github.com>
2023-07-26 10:06:05 -0400

    Add files via upload

---
+++ Sana /quiz/quiz/ViewController6.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController6.swift
+//  quiz
+//
+//  Created by SMART Scholars on 7/25/23.
+//
+
+import UIKit
+
+class ViewController6: UIViewController {
+
+    @IBOutlet weak var answerDisplay: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func response1(_ sender: UIButton) {
+        answerDisplay.text = "❌"
+    }
+    
+    @IBAction func response2(_ sender: UIButton) {
+        answerDisplay.text = "✅"
+    }
+    
+    @IBAction func response3(_ sender: UIButton) {
+        answerDisplay.text = "❌"
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
